5391a690c484e0e12b13449344c76832d5ca05e0
indigodarkwolf <indigo.darkwolf@gmail.com>
2022-12-01 00:16:03 -0600

    Day 1 solutions

---
+++ 1/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <queue>
+
+std::priority_queue<int> calories_set;
+
+int main()
+{
+	std::ifstream infile("input.txt", std::ios_base::in);
+	if (!infile.is_open()) {
+		return -1;
+	}
+
+	std::string line;
+
+	int calories = 0;
+	int elf = 1;
+	while (std::getline(infile, line)) {
+		std::cout << line << std::endl;
+		if(line.length() == 0) {
+			std::cout << elf << ": " << calories << std::endl;
+			calories_set.push(calories);
+
+			calories = 0;
+			++elf;
+		}
+
+		calories += atoi(line.c_str());
+	}
+
+	std::cout << "Most calories: " << calories_set.top() << std::endl;
+
+	int total = 0;
+	for(int i=0; i < 3; ++i) {
+		if(calories_set.empty()) {
+			break;
+		}
+		total += calories_set.top();
+		calories_set.pop();
+	}
+	std::cout << "Top 3 total: " << total << std::endl;
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aoc2022
 Advent of Code 2022
+Solutions by Stephen Horn
+https://adventofcode.com/2022
